e80acc50de0ac86d3d24cd2003a78fb76363891b
Deepakkashyap19 <kashyapdeepak1903@gmail.com>
2026-03-10 20:15:40 +0530

    Update chatbot.route.js

--- Backend/routes/chatbot.route.js
+++ Backend/routes/chatbot.route.js
@@ -2,6 +2,6 @@ import express from "express";
 import messageController from "../controllers/chatbot.message.js";
 const router = express.Router();
 
-router.post("/", messageController);
+router.post("https://chatbot-1-pngn.onrender.com/", messageController);
 
 export default router;
